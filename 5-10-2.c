2f3f4cc516775394c82cc0575cdf23efedbbdf5b
kaizawawa <kai109870@gmail.com>
2020-02-21 18:14:07 +0900

    echo program

--- 5-10-2.c
+++ 5-10-2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	while (--argc > 0)
 	{
@@ -9,4 +9,3 @@ int main (int argc, char *argv[])
 		return 0;
 	}
 }
-
